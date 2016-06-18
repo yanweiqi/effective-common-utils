f15ab2bfd0bb92c989ef438345a7ffe622e843b6
yanweiqi <yanweiqi@jd.com>
2016-06-18 10:57:09 +0800

    环境变量设置 Signed-off-by: yanweiqi <yanweiqi@jd.com>

---
+++ app-report/src/main/java/com/effective/common/domain/OSType.java
@@ -0,0 +1,5 @@
+package com.effective.common.domain;
+
+public enum OSType {
+	Windows,Mac,Linux,Other
+}
---
+++ app-report/src/main/java/com/effective/common/utils/ResourceUtils.java
@@ -0,0 +1,78 @@
+package com.effective.common.utils;
+
+import com.effective.common.domain.OSType;
+import com.google.common.io.Resources;
+
+public class ResourceUtils {
+	
+	private static OSType oSType;
+	private static String PROJECT_NAME = "hyperic-sigar-1.6.4";
+	private static String PROJECT_BIN = "sigar-bin";
+	private static String CLASS_PATH = "java.library.path";
+
+	/**
+	 * 在项目中获取“hyperic-sigar-1.6.4”的绝对路径
+	 * @return
+	 */
+	public static String getSigarPath(){
+		StringBuilder sigarPath = new StringBuilder();
+		String filePath = Resources.getResource(PROJECT_NAME).getFile().replaceFirst("/", "");
+		String separator = getOSType() == OSType.Windows ? "/" :"\\";
+		sigarPath.append(filePath)
+			.append(separator)
+			.append(PROJECT_BIN)
+			.append(separator)
+			.append("lib");
+		return sigarPath.toString();
+	}
+	
+	/**
+	 * 获取操作系统类型
+	 * @return
+	 */
+	public static OSType getOSType(){
+		if (oSType == null) {
+			String OS = System.getProperty("os.name", "generic").toLowerCase();
+			if (OS.indexOf("win") >= 0) {
+				oSType = OSType.Windows;
+			} else if ((OS.indexOf("mac") >= 0) || (OS.indexOf("darwin") >= 0)) {
+				oSType = OSType.Mac;
+			} else if (OS.indexOf("nux") >= 0) {
+				oSType = OSType.Linux;
+			} else {
+				oSType = OSType.Other;
+			}
+		}
+		return oSType;
+	}
+	
+	/**
+	 * 添加系统环境变量
+	 * @param name 
+	 * @param path
+	 */
+	public static void addSystemEnv(String name,String path,OSType oSType){
+		String classPath = getClassPath();
+		String newPath = new String(path);
+		if(getOSType() == OSType.Windows){
+			newPath += ";";
+		}
+		else{
+			newPath += ":";
+		}
+		newPath += classPath;
+		System.setProperty(name, newPath);
+	}
+	
+	public static String getClassPath(){
+		return System.getProperty(CLASS_PATH);
+	}
+	
+	public static void main(String[] args){
+		System.out.println(getSigarPath());
+		System.out.println(getOSType());
+		System.out.println(getClassPath());
+		addSystemEnv(CLASS_PATH,getSigarPath(),getOSType());
+		System.out.println(getClassPath());
+	}
+}
